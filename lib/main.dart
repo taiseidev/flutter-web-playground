5e1b347fa2b40f47d1b75c0a8d8d978ae1139040
onishi taisei <taisei.developer@gmail.com>
2023-09-11 02:24:22 +0900

    🚀 feat: Remove # path

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
   runApp(const MyApp());
 }
 
